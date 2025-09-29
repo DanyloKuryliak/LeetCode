cd6ff367979f5acdfbde40cfd34f7952efefa081
Danylo Kuryliak <danylo.kurylyak@gmail.com>
2025-09-29 18:16:19 +0300

    Added solution to IsIsomorphic.java

---
+++ IsIsomorphic.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Integer> hss = new HashMap<>();
+        Map<Character, Integer> hst = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!hss.containsKey(s.charAt(i))) {
+                hss.put(s.charAt(i), i);
+            }
+            if (!hst.containsKey(t.charAt(i))) {
+                hst.put(t.charAt(i), i);
+            }
+            if (!hss.get(s.charAt(i)).equals(hst.get(t.charAt(i)))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
