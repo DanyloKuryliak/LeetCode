398ffded606b8d8dae608acaf204c8d543730a75
Danylo Kuryliak <danylo.kurylyak@gmail.com>
2025-09-30 14:16:54 +0300

    Added KthSmallestElement Solution

---
+++ nodeproblems/KthSmallesElement.java
@@ -0,0 +1,18 @@
+package nodeproblems;
+
+import java.util.*;
+
+public class KthSmallesElement {
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> arr = inorder(root, new ArrayList<Integer>());
+        return arr.get(k-1);
+    }
+
+    private List<Integer> inorder(TreeNode root, List<Integer> arr) {
+        if (root == null) return arr;
+        inorder(root.left, arr);
+        arr.add(root.val);
+        inorder(root.right, arr);
+        return arr;
+    }
+}
