2d239aa08625f14ba3508ed1265ffe1cc3044269
Danylo Kuryliak <danylo.kurylyak@gmail.com>
2025-10-02 19:15:45 +0300

    Added 3 solutions to node problems

---
+++ nodeproblems/ValidateBST.java
@@ -0,0 +1,16 @@
+package nodeproblems;
+
+import javax.swing.tree.TreeNode;
+
+public class ValidateBST {
+    public boolean isValidBST(TreeNode root) {
+        return valid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean valid(TreeNode node, long min, long max) {
+        if (node == null) return true;
+
+        if (!(node.val > min && node.val < max)) return false;
+        return valid(node.left, min, node.val) && valid(node.right, node.val, max);
+    }
+}
