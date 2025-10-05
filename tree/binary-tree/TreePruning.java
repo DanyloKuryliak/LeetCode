453bb82c09cf0f90d180d959fb8990e21a07ab04
Danylo Kuryliak <danylo.kurylyak@gmail.com>
2025-10-05 14:32:29 +0300

    Added solution to TreePruning

---
+++ tree/binary-tree/TreePruning.java
@@ -0,0 +1,19 @@
+package tree.binarytree;
+import utils.TreeNode;
+import utils.ListNode;
+import java.util.*;
+public class TreePruning {
+    
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) return null;
+
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+
+        if (root.left == null && root.right == null && root.val == 0) {
+            return null;
+        } else {
+            return root;
+        }
+    }
+}
