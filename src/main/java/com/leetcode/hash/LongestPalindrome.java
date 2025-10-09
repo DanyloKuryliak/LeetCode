0b1c4b77f3b44ec210b383f77db739ef88372ab1
Danylo Kuryliak <danylo.kurylyak@gmail.com>
2025-10-09 19:22:26 +0300

    Added solution to LongestPalindrome.java

---
+++ src/main/java/com/leetcode/hash/LongestPalindrome.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class  LongestPalindrome {
+    public int longestPalindrome(String s) {
+        int oddfreq = 0;
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+            if (map.get(c) % 2 == 1) {
+                oddfreq++;
+            } else {
+                oddfreq--;
+            }
+        }
+
+        if (oddfreq > 0) {
+            return s.length() - oddfreq + 1;
+        } else {
+            return s.length();
+        }
+    }
+}
