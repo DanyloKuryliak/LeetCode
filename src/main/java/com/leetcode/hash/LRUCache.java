d6ba79d7b8ba41f18059fbe36d3c95fdfdb66eca
Danylo Kuryliak <danylo.kurylyak@gmail.com>
2025-10-09 19:04:58 +0300

    Added solution to LRUcache

---
+++ src/main/java/com/leetcode/hash/LRUCache.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+
+public class LRUCache {
+
+    private int final capacity;
+    private final LinkedHashMap<Integer, Integer> map;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.map = new LinkedHashMap<>(capacity, 0.75f, true);
+    }
+
+    public int get(int key) {
+        return map.getOrDefault(key, -1)
+    }
+
+    public void put(int key, int value) {
+        map.put(key, value);
+        if (map.size() > capacity) {
+            int oldestKey = map.keySet().iterator().next();
+            map.remove(oldestKey);
+        }
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
