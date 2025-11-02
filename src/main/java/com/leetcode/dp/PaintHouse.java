1e1af172e2bf96c6b80aae5dbfe2935aa20bc8a9
Danylo Kuryliak <danylo.kurylyak@gmail.com>
2025-11-02 20:34:21 +0200

    paint house

---
+++ src/main/java/com/leetcode/dp/PaintHouse.java
@@ -0,0 +1,21 @@
+package com.leetcode.dp;
+
+class PaintHouse {
+    public int minCost(int[][] costs) {
+        if (costs == null || costs.length == 0) return 0;
+        int n = costs.length;
+        int[][] dp = new int[n][3];
+
+        for (int j = 0; j < 3; j++) {
+            dp[0][j] = costs[0][j];
+        }
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = costs[i][0] + Math.min(dp[i-1][1],dp[i-1][2]);
+            dp[i][1] = costs[i][1] + Math.min(dp[i-1][0],dp[i-1][2]);
+            dp[i][2] = costs[i][2] + Math.min(dp[i-1][0],dp[i-1][1]);
+        }
+
+        return Math.min(dp[n-1][0], Math.min(dp[n-1][1], dp[n-1][2]));
+    }
+}
